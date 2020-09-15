d9e811663c3f6b627e919e5fed95cfe19028501c
Mirela Tomazini <70122909+mirelatomazini@users.noreply.github.com>
2020-09-15 16:00:04 -0300

    Create limpeza_vs1.ino
    Primeira parte da limpeza do código. Tenho que olhar novamente e adicionar laço para enviar os valores adquiridos para um vetor/computador

---
+++ limpeza_vs1.ino
@@ -0,0 +1,163 @@
+/*
+   STM32 Digital Oscilloscope
+   using the STM32F103C8 MCU and the NT35702 2.4 inch TFT display
+   https://www.gameinstance.com/post/80/STM32-Oscilloscope-with-FFT-and-SD-export
+
+    GameInstance.com
+    2016-2018
+*/
+//#include <Adafruit_ILI9341_8bit_STM.h> - Biblioteca para usar o display
+//#include <Adafruit_GFX.h> - Biblioteca de gráficos
+//#include <SPI.h> Serial Peripheral Interface ou SPI é um protocolo que permite a comunicação do microcontrolador com diversos outros componentes, formando uma rede.
+//É uma especificação de interface de comunicação série síncrona usada para comunicação de curta distância, principalmente em sistemas embarcados
+// #include "SdFat.h" -  sistema de arquivos para cartão SD.
+
+//#include <table_fft.h> - Biblioteca para usar a transformada rápida de Fourier
+//#include <cr4_fft_stm32.h> - Acredito ser para transpormada rápida de Fourier, mas não tenho certeza absoluta
+
+#include <SPI.h>
+#include <table_fft.h>
+#include <cr4_fft_stm32.h>
+
+// static const uint8_t seria similar a fazer #define em termos de memória, a diferência está na static const impõe um tipo ao valor,
+// o que pode ser útil para sobrecarga de função ou operações matemáticas.
+
+
+static const uint8_t SD_CHIP_SELECT = PB12;
+static const uint8_t TIME_BUTTON = PA15;
+static const uint8_t TRIGGER_BUTTON = PB10;
+static const uint8_t TEST_SIGNAL = PA8;
+static const uint8_t CHANNEL_1 = PB0;
+static const uint8_t CHANNEL_2 = PB1;
+
+
+static const uint16_t ADC_RESOLUTION = 4096; // units
+static const uint16_t BUFFER_SIZE = 1024; // bytes
+static const uint8_t TRIGGER_THRESOLD = 127; // units
+static const float ADC_SCREEN_FACTOR = (float)EFFECTIVE_VERTICAL_RESOLUTION / (float)ADC_RESOLUTION;
+static const float VCC_3_3 = 3.3; // volts
+
+const uint8_t DT_DT[]   = {4,       2,     1,     1,     1,     1,     1,     1,     1,     1,     1};
+const uint8_t DT_PRE[]  = {0,       0,     0,     0,     0,     0,     0,     0,     0,     0,     1};
+const uint8_t DT_SMPR[] = {0,       0,     0,     1,     2,     3,     4,     5,     6,     7,     7};
+const float DT_FS[]     = {2571, 2571,  2571,  1800,  1384,   878,   667,   529,   429,   143,  71.4};
+const float DT_DIV[]    = {3.9,  7.81, 15.63, 22.73, 29.41, 45.45, 55.55, 83.33, 95.24, 293.3, 586.6};
+
+
+uint8_t bk[SCREEN_HORIZONTAL_RESOLUTION];
+uint16_t data16[BUFFER_SIZE];
+uint32_t data32[BUFFER_SIZE];
+uint32_t y[BUFFER_SIZE];
+uint8_t time_base = 7;
+uint16_t i, j;
+uint8_t state = 0;
+uint16_t maxy, avgy, miny;
+
+volatile uint8_t h = 1, h2 = -1;
+volatile uint8_t trigger = 1, freeze = 0;
+volatile bool bPress[3], bTitleChange = true, bScreenChange = true;
+volatile static bool dma1_ch1_Active;
+
+
+// ------------------------------------------------------------------------------------
+// The following section was inspired by http://www.stm32duino.com/viewtopic.php?t=1145
+
+void setADCs() {
+  //
+  switch (DT_PRE[time_base]) {
+    //
+    case 0: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_2); break;
+    case 1: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_4); break;
+    case 2: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_6); break;
+    case 3: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_8); break;
+    default: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_8);
+  }
+  switch (DT_SMPR[time_base]) {
+    //
+    case 0: adc_set_sample_rate(ADC1, ADC_SMPR_1_5); break;
+    case 1: adc_set_sample_rate(ADC1, ADC_SMPR_7_5); break;
+    case 2: adc_set_sample_rate(ADC1, ADC_SMPR_13_5); break;
+    case 3: adc_set_sample_rate(ADC1, ADC_SMPR_28_5); break;
+    case 4: adc_set_sample_rate(ADC1, ADC_SMPR_41_5); break;
+    case 5: adc_set_sample_rate(ADC1, ADC_SMPR_55_5); break;
+    case 6: adc_set_sample_rate(ADC1, ADC_SMPR_71_5); break;
+    case 7: adc_set_sample_rate(ADC1, ADC_SMPR_239_5); break;
+    default: adc_set_sample_rate(ADC1, ADC_SMPR_239_5);
+  }
+  adc_set_reg_seqlen(ADC1, 1);
+  ADC1->regs->SQR3 = PIN_MAP[CHANNEL_1].adc_channel;
+  ADC1->regs->CR2 |= ADC_CR2_CONT; // | ADC_CR2_DMA; // Set continuous mode and DMA
+  ADC1->regs->CR2 |= ADC_CR2_SWSTART;
+}
+
+void real_to_complex(uint16_t * in, uint32_t * out, int len) {
+  //
+  for (int i = 0; i < len; i++) out[i] = in[i];
+}
+
+
+void inplace_magnitude(uint32_t * target, uint16_t len) {
+  //
+  uint16_t * p16;
+  for (int i = 0; i < len; i ++) {
+    //
+    int16_t real = target[i] & 0xFFFF;
+    int16_t imag = target[i] >> 16;
+    //    target[i] = 10 * log10(real*real + imag*imag);
+    uint32_t magnitude = asqrt(real * real + imag * imag);
+    target[i] = magnitude;
+  }
+}
+
+uint32_t perform_fft(uint32_t * indata, uint32_t * outdata, const int len) {
+  //
+  cr4_fft_1024_stm32(outdata, indata, len);
+  inplace_magnitude(outdata, len);
+}
+
+static void DMA1_CH1_Event() {
+  //
+  dma1_ch1_Active = 0;
+}
+
+void adc_dma_enable(const adc_dev * dev) {
+  //
+  bb_peri_set_bit(&dev->regs->CR2, ADC_CR2_DMA_BIT, 1);
+}
+// ------------------------------------------------------------------------------------
+
+void setup() {
+  //
+  tft.begin();
+  tft.setRotation(3);
+
+  bPress[0] = false;
+  bPress[1] = false;
+  bPress[2] = false;
+
+  adc_calibrate(ADC1);
+}
+
+void loop() {
+  //
+  // acquisition
+
+  setADCs();
+  dma_init(DMA1);
+  dma_attach_interrupt(DMA1, DMA_CH1, DMA1_CH1_Event);
+  adc_dma_enable(ADC1);
+  dma_setup_transfer(DMA1, DMA_CH1, &ADC1->regs->DR, DMA_SIZE_16BITS, data16, DMA_SIZE_16BITS, (DMA_MINC_MODE | DMA_TRNS_CMPLT));
+  dma_set_num_transfers(DMA1, DMA_CH1, BUFFER_SIZE);
+  dma1_ch1_Active = 1;
+  dma_enable(DMA1, DMA_CH1);                     // enable the DMA channel and start the transfer
+
+  while (dma1_ch1_Active) {};                    // waiting for the DMA to complete
+  dma_disable(DMA1, DMA_CH1);                    // end of DMA trasfer
+
+  real_to_complex(data16, data32, BUFFER_SIZE);  // data format conversion
+  perform_fft(data32, y, BUFFER_SIZE);           // FFT computation
+
+
+  delay(50);
+  state = 1;
+}
