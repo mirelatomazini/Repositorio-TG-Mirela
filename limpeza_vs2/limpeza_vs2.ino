7330a7a6aef1cff30d9ddae3c9ef3572434e581a
Mirela Tomazini <70122909+mirelatomazini@users.noreply.github.com>
2021-04-21 16:59:51 -0300

    Update limpeza_vs2.ino
    Reoção de comandos inuteis do código, essa versão compila, mas ainda não foi testada

--- limpeza_vs2/limpeza_vs2.ino
+++ limpeza_vs2/limpeza_vs2.ino
@@ -6,49 +6,29 @@
     GameInstance.com
     2016-2018
 */
-//#include <Adafruit_ILI9341_8bit_STM.h> - Biblioteca para usar o display
-//#include <Adafruit_GFX.h> - Biblioteca de gráficos
-//#include <SPI.h> Serial Peripheral Interface ou SPI é um protocolo que permite a comunicação do microcontrolador com diversos outros componentes, formando uma rede.
-//É uma especificação de interface de comunicação série síncrona usada para comunicação de curta distância, principalmente em sistemas embarcados
-// #include "SdFat.h" -  sistema de arquivos para cartão SD.
-
-//#include <table_fft.h> - Biblioteca para usar a transformada rápida de Fourier
-//#include <cr4_fft_stm32.h> - Acredito ser para transpormada rápida de Fourier, mas não tenho certeza absoluta
-//#include <table_fft.h>
 
 #include <STM32ADC.h>
-#include <SPI.h>
-#include <cr4_fft_stm32.h>
+
+
 
 // static const uint8_t seria similar a fazer #define em termos de memória, a diferência está na static const impõe um tipo ao valor,
 // o que pode ser útil para sobrecarga de função ou operações matemáticas.
 
 
-//static const uint8_t SD_CHIP_SELECT = PB12;
-//static const uint8_t TIME_BUTTON = PA15;
-//static const uint8_t TRIGGER_BUTTON = PB10;
-//static const uint8_t CHANNEL_2 = PB1;
-
-static const uint8_t TEST_SIGNAL = PA8;
 static const uint8_t CHANNEL_1 = PB0;
 
 
 static const uint16_t ADC_RESOLUTION = 4096; // units
 static const uint16_t BUFFER_SIZE = 1024; // bytes
-static const uint8_t TRIGGER_THRESOLD = 127; // units
-static const float ADC_SCREEN_FACTOR = (float)EFFECTIVE_VERTICAL_RESOLUTION / (float)ADC_RESOLUTION;
 static const float VCC_3_3 = 3.3; // volts
 
 const uint8_t DT_DT[]   = {4,       2,     1,     1,     1,     1,     1,     1,     1,     1,     1};
 const uint8_t DT_PRE[]  = {0,       0,     0,     0,     0,     0,     0,     0,     0,     0,     1};
 const uint8_t DT_SMPR[] = {0,       0,     0,     1,     2,     3,     4,     5,     6,     7,     7};
 const float DT_FS[]     = {2571, 2571,  2571,  1800,  1384,   878,   667,   529,   429,   143,  71.4};
-const float DT_DIV[]    = {3.9,  7.81, 15.63, 22.73, 29.41, 45.45, 55.55, 83.33, 95.24, 293.3, 586.6};
 
 
-//uint8_t bk[SCREEN_HORIZONTAL_RESOLUTION];
 uint16_t data16[BUFFER_SIZE];
-uint32_t data32[BUFFER_SIZE];
 uint32_t y[BUFFER_SIZE];
 uint8_t time_base = 7;
 uint8_t h;
@@ -56,9 +36,7 @@ uint16_t i, j;
 //uint8_t state = 0;
 uint16_t maxy, avgy, miny;
 
-volatile uint8_t h = 1, h2 = -1;
-//volatile uint8_t trigger = 1, freeze = 0;
-//volatile bool bPress[3], bTitleChange = true, bScreenChange = true;
+//volatile uint8_t h = 1, h2 = -1;
 volatile static bool dma1_ch1_Active;
 
 
@@ -93,24 +71,6 @@ void setADCs() {
   ADC1->regs->CR2 |= ADC_CR2_SWSTART;
 }
 
-void real_to_complex(uint16_t * in, uint32_t * out, int len) {
-  //
-  for (int i = 0; i < len; i++) out[i] = in[i];
-}
-
-
-void inplace_magnitude(uint32_t * target, uint16_t len) {
-  //
-  uint16_t * p16;
-  for (int i = 0; i < len; i ++) {
-    //
-    int16_t real = target[i] & 0xFFFF;
-    int16_t imag = target[i] >> 16;
-    //    target[i] = 10 * log10(real*real + imag*imag);
-    uint32_t magnitude = asqrt(real * real + imag * imag);
-    target[i] = magnitude;
-  }
-}
 
 static void DMA1_CH1_Event() {
   //
@@ -124,14 +84,6 @@ void adc_dma_enable(const adc_dev * dev) {
 // ------------------------------------------------------------------------------------
 
 void setup() {
-  //
-  tft.begin();
-  tft.setRotation(3);
-
-  bPress[0] = false;
-  bPress[1] = false;
-  bPress[2] = false;
-
   adc_calibrate(ADC1);
 }
 
@@ -150,14 +102,11 @@ void loop() {
 
   while (dma1_ch1_Active) {};                    // waiting for the DMA to complete
   dma_disable(DMA1, DMA_CH1);                    // end of DMA trasfer
-  
+
   for (h=0; h<=BUFFER_SIZE; h = h+1){
-    Serial.println(in[h]);
+    Serial.println(data16[h]);
   }
-  // Para esse for não é BUFFER_SIZE, pode ser: data16, data32, pois todos são vetores e têm o tamanho do BUFFER_SIZE. TAMBÉM FIQUE ATENTA AO QUE A REAL_TO_COMPLEX esta fazendo.
-  
-
-  delay(50);
-
+    
+  delay(1000);
   
 }
