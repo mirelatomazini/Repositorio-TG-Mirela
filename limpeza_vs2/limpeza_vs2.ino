62e5eeca835b9f9aaba84eb2987ae12caa4df46f
Mirela Tomazini <70122909+mirelatomazini@users.noreply.github.com>
2021-05-30 13:15:52 -0300

    Update limpeza_vs2.ino
    Versao compila e roda, ainda necessita de ajustes com comentarios e organizacao do codigo

--- limpeza_vs2/limpeza_vs2.ino
+++ limpeza_vs2/limpeza_vs2.ino
@@ -1,47 +1,29 @@
 /*
+   codigo baseado no desenvolvido por GameInstance.com 2016-2018:
    STM32 Digital Oscilloscope
    using the STM32F103C8 MCU and the NT35702 2.4 inch TFT display
-   https://www.gameinstance.com/post/80/STM32-Oscilloscope-with-FFT-and-SD-export
-
-    GameInstance.com
-    2016-2018
+   https://www.gameinstance.com/post/80/STM32-Oscilloscope-with-FFT-and-SD-export 
 */
 
-#include <STM32ADC.h>
+#include <STM32ADC.h> //inclui a biblioteca do STM32
 
+static const uint8_t CHANNEL_1 = PB0; //define o pino em que sera adquirido o sinal do canal 1
 
+static const uint16_t ADC_RESOLUTION = 4096; // units - define uma variavel com a resolucao do ADC
+static const uint16_t BUFFER_SIZE = 1024; // bytes - define uma variavel com informacao do tamanho do buffer
+static const float VCC_3_3 = 3.3; // volts - define uma variavel com valor de tensao maximo do microcontrolador
 
-// static const uint8_t seria similar a fazer #define em termos de memória, a diferência está na static const impõe um tipo ao valor,
-// o que pode ser útil para sobrecarga de função ou operações matemáticas.
+const uint8_t DT_PRE[]  = {0,       0,     0,     0,     0,     0,     0,     0,     1};
+const uint8_t DT_SMPR[] = {0,       1,     2,     3,     4,     5,     6,     7,     7};
+const float DT_FS[]     = {2571,    1800,  1384,  878,   667,   529,   429,   143,   71.4};
+//as linhas acima determinam vetores responsaveis por alterar os presets utilizados para velocidade de aquisicao
 
-
-static const uint8_t CHANNEL_1 = PB0;
-
-
-static const uint16_t ADC_RESOLUTION = 4096; // units
-static const uint16_t BUFFER_SIZE = 1024; // bytes
-static const float VCC_3_3 = 3.3; // volts
-
-const uint8_t DT_DT[]   = {4,       2,     1,     1,     1,     1,     1,     1,     1,     1,     1};
-const uint8_t DT_PRE[]  = {0,       0,     0,     0,     0,     0,     0,     0,     0,     0,     1};
-const uint8_t DT_SMPR[] = {0,       0,     0,     1,     2,     3,     4,     5,     6,     7,     7};
-const float DT_FS[]     = {2571, 2571,  2571,  1800,  1384,   878,   667,   529,   429,   143,  71.4};
-
-
-uint16_t data16[BUFFER_SIZE];
-uint32_t y[BUFFER_SIZE];
+uint16_t data16[BUFFER_SIZE]; // cria um vetor que possui a quantidade de dados igual ao buffer_size 1024
+uint16_t y[BUFFER_SIZE]; // cria outro vetor que possui a quantidade de dados igual ao buffer_size 1024
 uint8_t time_base = 7;
-uint8_t h;
-uint16_t i, j;
-//uint8_t state = 0;
-uint16_t maxy, avgy, miny;
 
-//volatile uint8_t h = 1, h2 = -1;
 volatile static bool dma1_ch1_Active;
 
-
-// ------------------------------------------------------------------------------------
-// The following section was inspired by http://www.stm32duino.com/viewtopic.php?t=1145
 
 void setADCs() {
   //
@@ -52,7 +34,8 @@ void setADCs() {
     case 2: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_6); break;
     case 3: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_8); break;
     default: rcc_set_prescaler(RCC_PRESCALER_ADC, RCC_ADCPRE_PCLK_DIV_8);
-  }
+  } // determina em qual dos casos deve ser selecionado o DT_PRE a depender do valor inserido na variavel time_base
+  
   switch (DT_SMPR[time_base]) {
     //
     case 0: adc_set_sample_rate(ADC1, ADC_SMPR_1_5); break;
@@ -64,34 +47,30 @@ void setADCs() {
     case 6: adc_set_sample_rate(ADC1, ADC_SMPR_71_5); break;
     case 7: adc_set_sample_rate(ADC1, ADC_SMPR_239_5); break;
     default: adc_set_sample_rate(ADC1, ADC_SMPR_239_5);
-  }
+  }// determina em qual dos casos deve ser selecionado o DT_SMPR a depender do valor inserido na variavel time_base
+  
   adc_set_reg_seqlen(ADC1, 1);
   ADC1->regs->SQR3 = PIN_MAP[CHANNEL_1].adc_channel;
-  ADC1->regs->CR2 |= ADC_CR2_CONT; // | ADC_CR2_DMA; // Set continuous mode and DMA
+  ADC1->regs->CR2 |= ADC_CR2_CONT; // | ADC_CR2_DMA; // set continuous mode and DMA
   ADC1->regs->CR2 |= ADC_CR2_SWSTART;
+  // realiza as configuracoes finais necessarias para que se inicie a aquisicao de sinal
 }
 
-
 static void DMA1_CH1_Event() {
-  //
-  dma1_ch1_Active = 0;
+  dma1_ch1_Active = 0; //variavel que altera o estado para 0, parando a coleta
 }
 
 void adc_dma_enable(const adc_dev * dev) {
-  //
   bb_peri_set_bit(&dev->regs->CR2, ADC_CR2_DMA_BIT, 1);
 }
-// ------------------------------------------------------------------------------------
 
 void setup() {
-  adc_calibrate(ADC1);
+  adc_calibrate(ADC1); //funcao que calibra o ADC
 }
 
 void loop() {
-  //
-  // acquisition
-
-  setADCs();
+  // funcao usada para realizar a aquisicao;
+  setADCs(); // chama a funcao que configura o ADC
   dma_init(DMA1);
   dma_attach_interrupt(DMA1, DMA_CH1, DMA1_CH1_Event);
   adc_dma_enable(ADC1);
@@ -103,8 +82,9 @@ void loop() {
   while (dma1_ch1_Active) {};                    // waiting for the DMA to complete
   dma_disable(DMA1, DMA_CH1);                    // end of DMA trasfer
 
+  int h;
   for (h=0; h<=BUFFER_SIZE; h = h+1){
-    Serial.println(data16[h]);
+    Serial.println(data16[h]*3.3/4096.0);
   }
     
   delay(1000);
