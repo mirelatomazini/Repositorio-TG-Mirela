14a2ba326515e996f914dae6acb77983cd138035
Mirela <mirelaoliveirat@gmail.com>
2021-11-07 19:11:47 -0300

    Termino do teste 3, adição da tabela de resultados e dos códigos utilizados para aquisição.

---
+++ arquivos_finais/software/testes/teste3/teste3mult/teste3mult.ino
@@ -0,0 +1,85 @@
+/*
+   codigo baseado no desenvolvido por GameInstance.com 2016-2018 e adaptador por Mirela Tomazini:
+   STM32 Digital Oscilloscope
+   using the STM32F103C8 MCU and the NT35702 2.4 inch TFT display
+   https://www.gameinstance.com/post/80/STM32-Oscilloscope-with-FFT-and-SD-export 
+*/
+
+/* a primeira parte do codigo importa as bibliotecas e cria variaveis globais */
+
+#define FREQ0 200000
+#define FREQ1 20000
+#define FREQ2 2000
+#define BUFFER_SIZE 600 
+#define VCC_3_3 3.3
+
+#include "adc_proj.h"
+#include "dft.h"
+
+int flag_inicial1         = 1;
+int flag_inicial2         = 1;
+int cont                  = 0;
+float fpeak1              = 0;
+float fpeak2              = 0;
+uint8_t time_base         = 2;
+float sample_freq         = DT_FS[time_base];
+static const uint8_t CHANNEL = PB0;                   //define o pino em que sera adquirido o sinal do canal 1
+uint16_t data16[BUFFER_SIZE];                          // cria um vetor que possui a quantidade de dados igual ao buffer_size
+
+
+void setup() {
+  digitalWrite(PA0, HIGH);
+  adc_calibrate(ADC1);                                  //funcao que calibra o ADC
+  pinMode(PA0, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+}
+
+
+void loop() {
+  // funcao usada para realizar a aquisicao;
+  setADCs(time_base, CHANNEL);                                         // chama a funcao que configura o ADC
+  DMA_(data16);                                            // chama a funcao que configura o DMA 
+  dma1_ch1_Active = 1;                               // muda o valor da variavel booleana para habilitar a coleta
+  dma_enable(DMA1, DMA_CH1);                         // habilita o canal DMA e inicia a transferencia
+  
+  while (dma1_ch1_Active) {};                        // aguardando a interrupcao mudar o valor da variavel booleana para encerrar a aquisicao
+  dma_disable(DMA1, DMA_CH1);                        // finaliza a aquisição 
+  
+
+  int h;                                         //cria uma variavel inteira 
+  if(1==2){
+    for (h=0; h<BUFFER_SIZE; h = h+1){            //cria um laco com o tamanho do buffer para realizar a tranferencia das informacoes         
+      Serial.print(data16[h]*VCC_3_3/4096.0);
+      Serial.print(" ");
+      Serial.println(data16[h]*VCC_3_3/4096.0);
+    }
+  }
+                                                // printa os valores adquiridos apos realizar a conversao para valores de tensao
+  if(1==1){
+    if(flag_inicial1){ 
+      if(digitalRead(PA0)){
+        fpeak1 = search_fpeak_initial_faster (data16, sample_freq, FREQ0);
+        flag_inicial1 = 0;  
+      }
+    }
+    if(flag_inicial2){
+      if(!digitalRead(PA0)){
+      fpeak2 = search_fpeak_initial_faster (data16, sample_freq, FREQ1);
+      flag_inicial2 = 0;   
+      }
+    }
+    if(digitalRead(PA0)){ 
+      fpeak1 = search_fpeak_initial(data16, sample_freq, 10000, 50, fpeak1-240, fpeak1+245,5,BUFFER_SIZE);
+    }
+    if(!digitalRead(PA0)){ 
+      fpeak2 = search_fpeak_initial(data16, sample_freq, 10000, 50, fpeak2-20,  fpeak2+25,5,BUFFER_SIZE);
+      delay(3000);
+    } 
+    
+    digitalWrite(PA0, !digitalRead(PA0));
+    
+  }
+}
